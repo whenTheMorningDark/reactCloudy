9cc05943c05f0968d2e1a64cdf0a404c04bc3462
huangkanhui <huangkanhui@ccssoft.com.cn>
2020-01-17 15:52:00 +0800

    huangkanghui

---
+++ src/application/Rank/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Rank(props) {
+	return <div> Rank </div>;
+}
+export default React.memo(Rank);
---
+++ src/application/Singers/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function singers(props) {
+	return <div> singers </div>;
+}
+export default React.memo(singers);
