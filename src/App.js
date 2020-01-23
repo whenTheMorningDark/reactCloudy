e30327230aef0c964ef7d947fa5e40000603424d
whenTheMorningDark <404792402@qq.com>
2020-01-23 22:59:27 +0800

    kafei 学习react

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import routes from './routes/index.js';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
-function App() {
+function App () {
 	return (
 		<Provider store={store}>
 			<HashRouter>
